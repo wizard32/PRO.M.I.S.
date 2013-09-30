16835ca6f84c946e9c0829562b5dd9443869cde7
wizard32 <liatsisfotis@gmail.com>
2013-09-30 13:06:06 +0300

    PRO.M.I.S. Code

---
+++ src/promis/Module.java
@@ -0,0 +1,20 @@
+public class Module {
+    public double moduleInput;
+    public double moduleOutput;
+    public int counter;
+    public double mean;
+
+   /* public Module(double input) {
+        this.moduleInput = input;
+        this.moduleOutput=0;
+    }*/
+
+    public Module(Object o) {
+
+    }
+
+    public Module() {
+        this.counter = 0;
+
+    }
+}
