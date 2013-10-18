03f0f15da1b4a8702cf86c48cd869e3a126ca884
wizard32 <liatsisfotis@gmail.com>
2013-10-18 22:05:20 +0300

    NGCE MakeFile - Starting File Updated

---
+++ ngcev_2.0/bin/ngce_start.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+clear
+cd ../classes/
+
+flag=c
+while test "$flag" != "q" ; do
+	
+	echo "User: $USER"
+	DATE=`date`
+	echo "$DATE. \n"
+	
+	echo "######################################################"
+	echo "# Network Graphs for Computer Epidemiologists (NGCE) #"
+	echo "######################################################"
+
+	echo "\n1: Build Graph"
+	echo "2: Post-processing of the Graph (Analyze)"
+	echo "3: Visualize the Graph structure"
+	echo "4: Exit"
+	echo "-----------------------------------------"
+	read -p  "Enter your selection: " answer
+	echo "-----------------------------------------"
+	
+	if test "$answer" = "1" ; then
+		java -Xmx300M BuildGraph
+	elif test "$answer" = "2" ; then
+		java -Xmx300M GraphExplorer
+		cd ../other/
+		gnuplot "plotGraph.plt"
+		cd ../classes/
+	elif test "$answer" = "3" ; then
+		java -Xmx300M PajekOut
+	elif test "$answer" = "4" ; then
+		echo "\nGood Bye..."
+		break
+	else
+		echo "\nWrong Parameter Selected"
+	fi
+	echo "\n"
+	echo "Do you want to continue? c(Continue) q(Quit)"
+	read -p  "Enter your choise: " flag
+	clear
+	if test "$flag" = "q" ; then
+		echo "\nGood Bye..."
+	fi
+
+done
