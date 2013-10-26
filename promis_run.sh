172d1317af505dd3c0a4e743aeb8c6fc1ea22b78
wizard32 <liatsisfotis@gmail.com>
2013-10-26 10:53:41 +0300

    PRO.M.I.S. Running File Updated

--- promis_run.sh
+++ promis_run.sh
@@ -16,6 +16,9 @@ while test "$flag" != "q" ; do
 
 	echo "\n1: Run PRO.M.I.S. Simulation"
 	echo "2: Configure config file"
+	echo "3: Create Required Folders"
+	echo "4: Remove Previous Simulation Results"
+	echo "5: Show GraphTopology Information"
 	echo "x:"
 	echo "n: Exit"
 	echo "-----------------------------------------"
@@ -23,11 +26,22 @@ while test "$flag" != "q" ; do
 	echo "-----------------------------------------"
 	
 	if test "$answer" = "1" ; then
-		
+		./xTimesPromis.sh 1 fotis
 	elif test "$answer" = "2" ; then
-
-	elif test "$answer" = "x" ; then
-
+		nano ../../bin/config.txt
+	elif test "$answer" = "3" ; then
+		./init.sh
+	elif test "$answer" = "4" ; then
+		./cleandata.sh
+	#elif test "$answer" = "x" ; then
+	elif test "$answer" = "5" ; then
+		clear
+		echo "GraphTopology Specifications"
+		echo "-----------------------------------------\n"
+		INFO=`cat ../../GraphTopology.txt  | head -5 | awk '{ print $0; }'`
+		echo "$INFO"
+		SIZE=$(ls -lah ../../GraphTopology.txt | awk '{ print $5}')
+		echo "#Size:= $SIZE" 
 	elif test "$answer" = "n" ; then
 		echo "\nGood Bye..."
 		break
